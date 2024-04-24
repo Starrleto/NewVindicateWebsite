e8bd2a4c7edc297c63440ac875c9b67832124056
Starrleto <ellierasuli@gmail.com>
2024-04-24 15:22:55 -0700

    mou ikkai

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -7,7 +7,7 @@ const HomeComponent = () => {
       
     <div className='home-bg'>
       <h1 className='title-pages'>Welcome To The Spirit World</h1>
-      <p>Vindicate is an RPG game developed by a computer science student with a love of mythology and storytelling.</p>
+      <p>Vindicate is a Metroidvania / RPG game developed by a computer science student with a love of mythology and storytelling.</p>
       <p>Although the game is in very early stages, please explore the content there currently is to offer and learn about our project!</p>
     </div>
     
--- src/Components/UpdatesComponent.js
+++ src/Components/UpdatesComponent.js
@@ -3,8 +3,39 @@ import "../App.css";
 
 const UpdatesComponent = () => {
   return (
-    <div>UpdatesComponent</div>
+    <div>
+      <h1 className='title-pages'>Updates</h1>
+      <p>Follow the development process!</p>
+
+      <div className='body-padding'>
+
+        <div>
+          <h1 className='date'>2/18-19/2024</h1>
+          <p className='text-left'>I tested out my cutscene system here. The intro and beginning cutscenes now exist, though they might change. I sketched out a CG to test that out too, it functions well!</p>
+          <p className='text-left'>I've also added a little popup mechanic for tutorial purposes.</p>
+        </div>
+<hr></hr>
+        <div>
+          <h1 className='date'>3/4/2024</h1>
+          <p className='text-left'>Multiple save slots have been added! Currently there are three.</p>
+          <p className='text-left'>It saves money, inventory, and player position. I will add progression and stat saving later.</p>
+        </div>
+<hr></hr>
+        <div>
+          <h1 className='date'>4/24-25/2024</h1>
+          <p className='text-left'>For these days I worked on the battle system. I've set up several classes and the basic UI for the battles.</p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
+
+{/* 
+
+<p className='text-left'></p>
+
+*/}
 
 export default UpdatesComponent
